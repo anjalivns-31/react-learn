7876f7eb32170610c753f6be1b42d304133ccfa2
Anjali Verma <anjalivermavns2@gmail.com>
2026-01-19 14:28:02 +0530

    added 02-components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/navbar'
+
+
+const App = () => {
+  return (
+    <>
+    {Card()}
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>hello anjali</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, vel.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
